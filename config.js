7d7a8f37dc5f8397cce7b3834f6d219afed51cf7
riyoxdz <101568530+RiyoXdz@users.noreply.github.com>
2022-06-28 14:17:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-global.thumb = fs.readFileSync('./lib/minami.jpg')
+global.thumb = fs.readFileSync('./lib/apri.jpg')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
